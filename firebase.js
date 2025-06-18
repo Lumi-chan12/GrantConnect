110bdd7dee2ef2e948351c555b51ad4dbd5df03b
Lumi-chan12 <santoshihajare20@gmail.com>
2025-06-18 22:18:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.grant-connect-c2317,
-    privateKey: process.env.privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
     clientEmail: process.env.firebase-adminsdk-fbsvc@grant-connect-c2317.iam.gserviceaccount.com,
   }),
 });
