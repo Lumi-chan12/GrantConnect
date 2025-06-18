6e8ddbcbcf4a6c368d2926e2c9a47f91659859cb
Lumi-chan12 <santoshihajare20@gmail.com>
2025-06-18 22:25:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,20 @@
-const admin = require("firebase-admin");
+// firebase.js
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "token_uri": "https://oauth2.googleapis.com/token"
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.grant-connect-c2317,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
-    clientEmail: process.env.firebase-adminsdk-fbsvc@grant-connect-c2317.iam.gserviceaccount.com,
-  }),
+  credential: admin.credential.cert(serviceAccount)
 });
 
-module.exports = admin;
+export default admin;
+
